ac0e4cc2b0ac16bf62854fa90915ca369ea1f8b0
Annareddy Jagadeswar Reddy <100749580+ANNAREDDY123@users.noreply.github.com>
2026-07-06 09:57:24 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,45 @@
+import logging
+
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+from database import Base, engine
+
+from routes.auth import router as auth_router
+from routes.customers import router as customers_router
+from routes.tickets import router as tickets_router
+
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s %(levelname)s %(message)s"
+)
+
+logger = logging.getLogger(__name__)
+
+Base.metadata.create_all(bind=engine)
+
+app = FastAPI(
+    title="Complaint & Ticket Management System"
+)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
+
+app.include_router(auth_router)
+app.include_router(customers_router)
+app.include_router(tickets_router)
+
+
+@app.get("/")
+def home():
+
+    logger.info("Application Started Successfully")
+
+    return {
+        "message": "Complaint & Ticket Management System"
+    }
