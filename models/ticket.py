915dd73dfdf639ad7ebfe74726fce96d9c9c8354
Annareddy Jagadeswar Reddy <100749580+ANNAREDDY123@users.noreply.github.com>
2026-07-06 09:50:09 +0530

    Create ticket.py

---
+++ models/ticket.py
@@ -0,0 +1,42 @@
+from sqlalchemy import (
+    Column,
+    Integer,
+    String,
+    ForeignKey
+)
+
+from database import Base
+
+
+class Ticket(Base):
+
+    __tablename__ = "tickets"
+
+    id = Column(
+        Integer,
+        primary_key=True
+    )
+
+    customer_id = Column(
+        Integer,
+        ForeignKey("customers.id")
+    )
+
+    assigned_agent_id = Column(
+        Integer,
+        ForeignKey("users.id"),
+        nullable=True
+    )
+
+    title = Column(String)
+
+    description = Column(String)
+
+    priority = Column(String)
+
+    category = Column(String)
+
+    status = Column(
+        String,
+        default="Open"
+    )
