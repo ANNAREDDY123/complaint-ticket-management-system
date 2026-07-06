56b55c099e1dc04a0654ed3419735af9cce43fe4
Annareddy Jagadeswar Reddy <100749580+ANNAREDDY123@users.noreply.github.com>
2026-07-06 16:44:38 +0530

    Update database.py

--- database.py
+++ database.py
@@ -6,9 +6,7 @@ DATABASE_URL = "sqlite:///./complaint_ticket.db"
 
 engine = create_engine(
     DATABASE_URL,
-    connect_args={"check_same_thread": False}
-)
-
+    connect_args={"check_same_thread": False})
 SessionLocal = sessionmaker(
     autocommit=False,
     autoflush=False,
